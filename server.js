c9fcb8554d3d36ed7a6d61d37314d56ff89ace7a
PedroThiagoRoque <pedronmroque@gmail.com>
2023-10-25 01:26:15 -0300

    Chatbot funcional com limitação de escopo
    Usando prompt engineering para limitar o escopo das respostas do chatbot.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+
+app.use('/', routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+//Roda com node server.js
+//adicione sua chave de api do openai no .env
